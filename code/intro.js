89fc8a24d34b723be0e4075a758c425dc0ee0c70
kuzma <kuzmaka@gmail.com>
2022-06-26 00:28:04 +0300

    initial file structure

---
+++ code/intro.js
@@ -0,0 +1,6 @@
+export default () => {
+    add([
+        pos(center()),
+        sprite('bean')
+    ])
+}
--- code/main.js
+++ code/main.js
@@ -1,4 +1,11 @@
 import kaboom from "kaboom"
+import loadAssets from './loadAssets.js'
+import intro from './intro.js'
 
 // initialize context
 kaboom()
+
+loadAssets()
+
+scene('intro', intro)
+go('intro')
