e1003d90a80a1d5733197862daf62324e723d3d9
XpomAE <108197619+xpomea@users.noreply.github.com>
2022-06-25 21:08:46 +0000

    asdf

--- code/main.js
+++ code/main.js
@@ -2,27 +2,3 @@ import kaboom from "kaboom"
 
 // initialize context
 kaboom()
-
-// load assets
-loadSprite("bean", "sprites/bean.png")
-
-// add a character to screen
-add([
-	// list of components
-	sprite("bean"),
-	pos(80, 40),
-	area(),
-])
-
-add([
-  pos(0),
-  text('hello')
-])
-
-// add a kaboom on mouse click
-onClick(() => {
-	addKaboom(mousePos())
-})
-
-// burp on "b"
-onKeyPress("b", burp)
