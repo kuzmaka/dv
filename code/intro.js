21f386b23860f4827898c9e841aa337e828d6efd
kuzma <kuzmaka@gmail.com>
2022-06-26 10:09:03 +0300

    dizz cam effect

--- code/intro.js
+++ code/intro.js
@@ -27,13 +27,44 @@ function wakeUp() {
         color(BLACK),
         opacity(1)
     ])
-    onUpdate(() => {
+    let dizzed = false
+    const cancel = onUpdate(() => {
         if (cover2.opacity > 0) {
             cover2.opacity = Math.max(0, cover2.opacity - 0.3*dt())
         } else {
             if (cover1.opacity > 0) {
+                if (!dizzed) {
+                    camDizz()
+                    dizzed = true
+                }
                 cover1.opacity = Math.max(0, cover1.opacity - 0.3 * dt())
+            } else {
+                cancel()
             }
+        }
+    })
+}
+
+function camDizz()
+{
+    let angle = 0
+    let scale = 10
+    const cancel = onUpdate(() => {
+        if (angle < 360) {
+            angle += 120 * dt()
+        } else {
+            angle = 360
+        }
+        if (scale > 1) {
+            scale -= 3 * dt()
+        } else {
+            scale = 1
+        }
+        camRot(angle)
+        camScale(scale)
+        if (angle === 360 && scale === 1) {
+            camRot(0)
+            cancel()
         }
     })
 }
